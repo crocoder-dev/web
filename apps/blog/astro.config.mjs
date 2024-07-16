35cb3caf2d0a4a0f800caed05377939befb7d7ee
David Abram <david@crocoder.dev>
2024-07-16 11:45:56 +0200

    images

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -2,8 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import remarkToc from 'remark-toc';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
+  output: 'static',
+  adapter: vercel({
+    imageService: true,
+    imagesConfig: {
+      sizes: [640, 768, 1024, 1280],
+      domains: ['*'],
+    },
+    devImageService: 'shrap',
+  }),
   integrations: [tailwind(), react()],
   markdown: {
     remarkPlugins: [remarkToc],
