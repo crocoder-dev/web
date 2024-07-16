c42aa7b12767bda59e9728d92dc550d2008fe856
David Abram <david@crocoder.dev>
2024-07-16 11:00:24 +0200

    added react

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import remarkToc from 'remark-toc';
+import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()]
   markdown: {
     remarkPlugins: [remarkToc],
   },
