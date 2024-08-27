a00b2ef2ea9b347e4580f110e6883047a824022a
Luka <luka.sesar99@gmail.com>
2024-08-27 18:22:51 +0200

    Fix: feed get function

--- apps/blog/src/pages/rss.xml.ts
+++ apps/blog/src/pages/rss.xml.ts
@@ -3,33 +3,22 @@ import { getCollection } from "astro:content";
 
 const markdownPosts = await getCollection("posts");
 
-const authorsCollection = await getCollection("authors");
-
 const prefix = import.meta.env.DEV ? "/" : "/blog";
-
-const authorMap = authorsCollection.reduce(
-  (acc, author) => {
-    acc[author.slug] = author.data.name;
-    return acc;
-  },
-  {} as { [slug: string]: string },
-);
 
 const posts = markdownPosts.map((post) => ({
   url: `${prefix}${prefix === "/" ? "" : "/"}${post.slug}`,
   title: post.data.title,
   date: new Date(post.data.date),
   description: post.data.description || "",
-  authors: post.data.authors.map((slug: string) => authorMap[slug] || slug),
 }));
 
 posts.sort((a, b) => b.date.getTime() - a.date.getTime());
 
-export const get = () =>
+export const GET = () =>
   rss({
     title: "My Blog RSS Feed",
     description: "Latest posts from my blog",
-    site: import.meta.env.SITE,
+    site: 'https://www.crocoder.dev/blog',
     items: posts.map((post) => ({
       title: post.title,
       description: post.description,
