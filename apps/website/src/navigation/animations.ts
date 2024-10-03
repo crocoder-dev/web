973696ccc867b80f78d723e12c2e83dcbee0ab36
David Abram <david@crocoder.dev>
2024-10-03 16:38:01 +0200

    rewrite nav to astro

--- apps/website/src/navigation/animations.ts
+++
@@ -1,7 +0,0 @@
-const transition = `transition-all duration-300`;
-
-export const applyDesktopShrinkAnimation = (apply: boolean) => {
-  return apply
-    ? `md:!max-w-0 md:!mx-0 md:overflow-hidden md:${transition}`
-    : `md:max-w-48 md:${transition}`;
-};
