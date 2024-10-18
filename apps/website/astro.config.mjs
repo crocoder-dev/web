1b0ef967b6cd5f50207c8abd8c766480ec2874dc
stefanskoricdev <stephanskoric@gmail.com>
2024-10-18 18:35:53 +0200

    feat: add astro adapter

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,7 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react'
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
+  output: "static",
+  adapter: vercel({
+    imageService: true,
+  }),
   integrations: [tailwind(), react()],
 });
