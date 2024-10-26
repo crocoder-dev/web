f5064d1670800754d77ab16720010ee70d2e1ff7
stefanskoricdev <stephanskoric@gmail.com>
2024-10-26 18:38:22 +0200

    feat: Fix env vars, add footer and small styling updates

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
 
 export default defineConfig({
   output: "hybrid",
   adapter: vercel(),
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
 });
--- apps/website/src/env.d.ts
+++ apps/website/src/env.d.ts
@@ -1,2 +1,18 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly NOTION_TOKEN: string;
+  readonly SLACK_CHANNEL: string;
+  readonly IS_OFFLINE: string;
+  readonly SLACK_BOT_TOKEN: string;
+  readonly MENTION_EMAILS: string;
+  readonly MENTION_IDS: string;
+  readonly NOTION_DATABASE_ID: string;
+  readonly UPSTASH_REDIS_REST_URL: string;
+  readonly UPSTASH_REDIS_REST_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
