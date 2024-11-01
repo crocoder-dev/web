f169c74482f6913199003a15b184462dc09fb375
davidabram <david@crocoder.dev>
2024-11-01 18:29:44 +0100

    remove nonsense

--- apps/blog/astro.config.js
+++ apps/blog/astro.config.js
@@ -5,6 +5,12 @@ import vercel from '@astrojs/vercel/static';
 import createRemarkPlugin from '@crocoder-dev/remark-plugin';
 import { classes } from "./astro-tailwind.config";
 
+const classes = {
+  titleClass: 'font-bold text-[1.25rem] mt-[2.5rem]',
+  summaryClass: 'cursor-pointer font-bold text-[1.25rem]',
+  detailsClass: 'mt-[2.5rem]',
+}
+
 const remarkPlugin = createRemarkPlugin(classes);
 
 export default defineConfig({
