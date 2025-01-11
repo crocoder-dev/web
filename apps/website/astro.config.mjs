2f73ace6f7c6a4abbe7a0c741bd9e7ee2b4acd45
Luka <luka.sesar99@gmail.com>
2025-01-11 12:51:02 +0100

    Update: removed env siteurl

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -2,15 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from "@astrojs/sitemap";
 
-const siteUrl = import.meta.env.SITE_URL;
-
 // https://astro.build/config
 export default defineConfig({
-  site: siteUrl,
+  site: 'https://www.crocoder.dev/',
   integrations: [tailwind(), sitemap({
     lastmod: new Date(),
     changefreq: 'monthly',
     priority: 0.85,
-    customPages: [`${siteUrl}blog`],
-  })]
+    customPages: ['https://www.crocoder.dev/blog'],
+  })],
+  redirects: {
+    '/sitemap': '/sitemap.xml'
+  },
 });
