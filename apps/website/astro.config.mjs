c696184f00a9a8b2cc89ea2e53a33547221c0dcd
gloria <gloria@crocoder.dev>
2025-01-22 12:52:24 +0100

    fix[web-27]: remove prerednder

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,7 +1,7 @@
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
   output: "static",
