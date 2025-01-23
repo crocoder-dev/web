428c1100c1a123639cdf6d62bd6eab22599f390a
gloria <gloria@crocoder.dev>
2025-01-23 11:44:19 +0100

    fix[web-27]: add server proxy

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,34 +1,33 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/static';
-import createRemarkPlugin from '@crocoder-dev/remark-plugin';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/static";
+import createRemarkPlugin from "@crocoder-dev/remark-plugin";
 
 const classes = {
-  titleClass: 'font-bold text-[1.25rem] mt-[2.5rem]',
-  summaryClass: 'cursor-pointer font-bold text-[1.25rem]',
-  detailsClass: 'mt-[2.5rem]',
-  iframeClass: 'border-none w-full h-[380px] overflow-y-hidden'
-}
+  titleClass: "font-bold text-[1.25rem] mt-[2.5rem]",
+  summaryClass: "cursor-pointer font-bold text-[1.25rem]",
+  detailsClass: "mt-[2.5rem]",
+  iframeClass: "border-none w-full h-[380px] overflow-y-hidden",
+};
 
 const remarkPlugin = createRemarkPlugin(classes);
 
 export default defineConfig({
-  output: 'static',
+  output: "static",
   adapter: vercel({
     imageService: true,
     imagesConfig: {
       sizes: [640, 936],
-      domains: ['*'],
+      domains: ["*"],
     },
   }),
   redirects: {
-    '/feed': '/rss.xml',
-    '/sitemap': '/sitemap.xml'
+    "/feed": "/rss.xml",
+    "/sitemap": "/sitemap.xml",
   },
   integrations: [tailwind(), react()],
   markdown: {
     remarkPlugins: [remarkPlugin],
   },
 });
-
--- apps/website/src/env.d.ts
+++ apps/website/src/env.d.ts
@@ -1,10 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_CONTACT_URL: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
