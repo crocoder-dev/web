282631c86c07423a4cbbe81850f905448e350217
gloria <gloria@crocoder.dev>
2025-01-23 10:38:23 +0100

    feat[web-27]: change contact url

--- apps/website/src/env.d.ts
+++ apps/website/src/env.d.ts
@@ -2,15 +2,7 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly NOTION_TOKEN: string;
-  readonly SLACK_CHANNEL: string;
-  readonly IS_OFFLINE: string;
-  readonly SLACK_BOT_TOKEN: string;
-  readonly MENTION_EMAILS: string;
-  readonly MENTION_IDS: string;
-  readonly NOTION_DATABASE_ID: string;
-  readonly UPSTASH_REDIS_REST_URL: string;
-  readonly UPSTASH_REDIS_REST_TOKEN: string;
+  readonly CONTACT_URL: string;
 }
 
 interface ImportMeta {
