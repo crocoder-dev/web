12737cf400cef0df1cd76bc434b6d4f3d3a3c437
Luka <luka.sesar99@gmail.com>
2025-02-05 17:50:05 +0100

    added partials to feed and sitemap (#338)

--- apps/website/src/pages/rss.xml.ts
+++ apps/website/src/pages/rss.xml.ts
@@ -3,22 +3,40 @@ import { getCollection } from "astro:content";
 
 const markdownPosts = await getCollection("posts");
 
-const posts = markdownPosts.map((post) => ({
-  url: `/blog/${post.slug}`,
-  title: post.data.title,
-  createdAt: new Date(post.data.createdAt),
-  description: post.data.description || "",
-}));
+const postImportResult = import.meta.glob("./blog/**/*.astro", { eager: true });
+const pages = Object.values(postImportResult).filter(
+  (page: any) =>
+    !page.url?.includes("[") &&
+    !page.url?.includes("index.astro") &&
+    !(page.url === "/blog"),
+);
+
+const posts = [
+  ...markdownPosts.map((post: any) => ({
+    url: `/blog/${post.slug}`,
+    title: post.data.title,
+    createdAt: new Date(post.data.createdAt),
+    description: post.data.description || "",
+  })),
+  ...pages.map((post: any) => ({
+    url: post.url,
+    title: post.data.title,
+    createdAt: new Date(post.data.createdAt),
+    description: post.data.description || "",
+  })),
+];
 
 posts.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
 
 export const GET = () =>
   rss({
-    customData: '<atom:link href="https://www.crocoder.dev/blog/feed" rel="self" type="application/rss+xml" />',
+    customData:
+      '<atom:link href="https://www.crocoder.dev/blog/feed" rel="self" type="application/rss+xml" />',
     xmlns: { atom: "http://www.w3.org/2005/Atom" },
     title: "CroCoder | Blog",
-    description: "Tips and ideas to help you learn, build and improve your projects.",
-    site: 'https://www.crocoder.dev/blog',
+    description:
+      "Tips and ideas to help you learn, build and improve your projects.",
+    site: "https://www.crocoder.dev/blog",
     items: posts.map((post) => ({
       title: post.title,
       description: post.description,
