39caafb5d0a2e2faa9213a554cdb7a0843c9f6a9
ivke995 <ivanivic842@gmail.com>
2025-02-14 12:21:05 +0100

    redesign hero section

--- apps/website/src/components/_grid.ts
+++ apps/website/src/components/_grid.ts
@@ -1,6 +1,10 @@
 export const grid_classes = `
 grid
 grid-cols-6 md:grid-cols-12
-gap-x-3 lg:gap-x-7 
-gap-y-7 lg:gap-y-12 
+gap-x-3 lg:gap-x-7
+gap-y-7 lg:gap-y-12
 `;
+
+export const hero_grid_classes = `
+grid grid-cols-6 md:grid-cols-12
+lg:gap-x-7 lg:gap-y-12`
