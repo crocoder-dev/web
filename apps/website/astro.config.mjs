4c2efe7c39e83d401c9611ae90278601b0f9eae9
davidabram <david@crocoder.dev>
2025-07-19 10:07:20 +0200

    changes

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
 import createRemarkPlugin from '@crocoder-dev/remark-plugin';
 import image from '@astrojs/image';
 
