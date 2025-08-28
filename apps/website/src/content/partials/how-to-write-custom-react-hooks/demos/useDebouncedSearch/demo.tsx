71a6cc21e196fc5637197bb495851ca1539b344d
gloria <gloria@crocoder.dev>
2025-08-28 12:30:18 +0200

    chore: run prettier format on all files except .md

--- apps/website/src/content/partials/how-to-write-custom-react-hooks/demos/useDebouncedSearch/demo.tsx
+++ apps/website/src/content/partials/how-to-write-custom-react-hooks/demos/useDebouncedSearch/demo.tsx
@@ -1,24 +1,27 @@
 // Use the hook in a component
-import React, { useState } from 'react';
-import { useDebouncedSearch } from './useDebouncedSearch';
+import React, { useState } from "react";
+import { useDebouncedSearch } from "./useDebouncedSearch";
 
 const UseDebouncedSearchDemo: React.FC = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleSearch = (value: string) => {
     console.log(`Searching for: ${value}`);
   };
 
-  const [debouncedSearchTerm] = useDebouncedSearch(searchTerm, handleSearch, 500);
-
+  const [debouncedSearchTerm] = useDebouncedSearch(
+    searchTerm,
+    handleSearch,
+    500,
+  );
 
   return (
-    <div className='border rounded-md p-4'>
+    <div className="border rounded-md p-4">
       <input
-        className='rounded min-w-52 max-w-full'
+        className="rounded min-w-52 max-w-full"
         type="text"
         value={searchTerm}
-        onChange={e => setSearchTerm(e.target.value)}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <p>Debounced search term: {debouncedSearchTerm}</p>
     </div>
