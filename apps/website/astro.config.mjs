f48505e35475ad79ce5793b03bdfca9fd41de94a
Stefan Škorić <68769671+stefanskoricdev@users.noreply.github.com>
2025-08-29 16:34:49 +0200

    Feat image optimization (#587)
    * feat: Setup image optimization config
    
    * fix: image sizes
    
    * fix: sizes
    
    * feat: Image optimizations for landing page
    
    * fix: Service card image styling issue
    
    * fix: add 1920 size to image config
    
    * fix: set width to logo images
    
    * feat: Optimize images on Contact us
    
    * feat: Optimize images on blog index and post page
    
    * fix: correct typo

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,31 +1,34 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import vercel from '@astrojs/vercel'
-import createRemarkPlugin from '@crocoder-dev/remark-plugin';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
+import createRemarkPlugin from "@crocoder-dev/remark-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
 const classes = {
-  titleClass: 'font-bold text-[1.25rem] mt-[2.5rem]',
-  summaryClass: 'cursor-pointer font-bold text-[1.25rem]',
-  detailsClass: 'mt-[2.5rem]',
-  iframeClass: 'border-none w-full h-[360px] overflow-y-hidden'
-}
+  titleClass: "font-bold text-[1.25rem] mt-[2.5rem]",
+  summaryClass: "cursor-pointer font-bold text-[1.25rem]",
+  detailsClass: "mt-[2.5rem]",
+  iframeClass: "border-none w-full h-[360px] overflow-y-hidden",
+};
 
 const remarkPlugin = createRemarkPlugin(classes);
 
 export default defineConfig({
   prefetch: true,
-  output: 'server',
+  output: "server",
   adapter: vercel({
     imageService: true,
+    imagesConfig: {
+      sizes: [320, 480, 578, 640, 720, 800, 940, 1920],
+      formats: ["image/avif", "image/webp"],
+    },
   }),
   redirects: {
-    '/feed': '/rss.xml',
-    '/sitemap': '/sitemap.xml'
+    "/feed": "/rss.xml",
+    "/sitemap": "/sitemap.xml",
   },
   integrations: [tailwindcss(), react()],
   markdown: {
     remarkPlugins: [remarkPlugin],
   },
 });
-
