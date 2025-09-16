317a0e83835f1b6f2efbfeb316762ca5b131563a
Ante Koceić <ante@crocoder.dev>
2025-09-16 10:31:44 +0200

    Added tests for link to blog posts

--- tests/playwright/landingPageMobile.spec.ts
+++ tests/playwright/landingPageMobile.spec.ts
@@ -5,6 +5,8 @@ import type { Page } from "@playwright/test";
 const baseUrl = "https://www.crocoder.dev";
 const bookACallSection = "book-a-call-section";
 const discoverOurServices = "discover-our-services";
+const blogUrl = "blog";
+const contactUrl = "contact";
 
 test.describe("Landing page in mobile view", () => {
   let page: Page;
@@ -72,5 +74,76 @@ test.describe("Navigation via links in mobile view to", () => {
 
     await expect(page).toHaveURL(`${baseUrl}/#${discoverOurServices}`);
     await expect(page.locator(`#${discoverOurServices}`)).toBeInViewport();
+  });
+
+  test("how we rebuilt a legacy ui with zero downtime blog post", async () => {
+    const caseStudy = page
+      .locator("#animation-wrapper section")
+      .filter({ hasText: "Expert Led Digital" });
+
+    await caseStudy.scrollIntoViewIfNeeded();
+    await expect(caseStudy.getByRole("link")).toBeInViewport();
+    await caseStudy.getByRole("link").click();
+
+    await Promise.all([
+      page.waitForURL(
+        `${baseUrl}/${blogUrl}/how-we-rebuilt-a-legacy-ui-with-zero-downtime`,
+      ),
+      page.getByText(
+        "How We Rebuilt a Legacy UI With Zero Downtime: A Case Study in Component Libraries and Frontend Guidance",
+      ),
+    ]);
+  });
+
+  test("using lago to create a flexible billing system blog post", async () => {
+    const caseStudy = page
+      .locator("#animation-wrapper section")
+      .filter({ hasText: "Product & Platform" });
+
+    await caseStudy.scrollIntoViewIfNeeded();
+    await expect(caseStudy.getByRole("link")).toBeInViewport();
+    await caseStudy.getByRole("link").click();
+
+    await Promise.all([
+      page.waitForURL(
+        `${baseUrl}/${blogUrl}/using-lago-to-create-a-flexible-billing-system`,
+      ),
+      page.getByText("Using Lago to Create a Flexible Billing System"),
+    ]);
+  });
+
+  test("Contact us page", async () => {
+    const contactUsLink = page.getByRole("link", {
+      name: "Schedule a free DevEx audit",
+    });
+
+    await page
+      .locator("#animation-wrapper section")
+      .filter({ hasText: "Workflow & Release" })
+      .scrollIntoViewIfNeeded();
+    await expect(contactUsLink).toBeInViewport();
+    await contactUsLink.click();
+
+    await Promise.allSettled([
+      page.waitForURL(`${baseUrl}/${contactUrl}`, { timeout: 10000 }),
+      expect(page.getByText("get in touch")).toBeVisible(),
+    ]);
+  });
+
+  test("migrating an enterprise app from angularjs to react blog post", async () => {
+    const caseStudy = page
+      .locator("#animation-wrapper section")
+      .filter({ hasText: "Team Enablement & Upskilling" });
+
+    await caseStudy.scrollIntoViewIfNeeded();
+    await expect(caseStudy.getByRole("link")).toBeInViewport();
+    await caseStudy.getByRole("link").click();
+
+    await Promise.all([
+      page.waitForURL(
+        `${baseUrl}/${blogUrl}/migrating-an-enterprise-app-from-angularjs-to-react`,
+      ),
+      page.getByText("Migrating an Enterprise App from AngularJS to React"),
+    ]);
   });
 });
