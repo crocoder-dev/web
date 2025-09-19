02527d31c75edfef26604544630c7d945888570e
gloria <gloria@crocoder.dev>
2025-09-19 13:05:06 +0200

    feat: add email blog section

--- apps/contact/email-templates/contact.tsx
+++ apps/contact/email-templates/contact.tsx
@@ -1,4 +1,5 @@
-import { Text, Section } from "@react-email/components";
+import { Hr, Section, Text } from "@react-email/components";
+import Blog from "./components/Blog";
 import Layout from "./components/Layout";
 
 const ContactTemplate = () => {
@@ -24,6 +25,30 @@ const ContactTemplate = () => {
           Best regards, <br />
           The CroCoder team
         </Text>
+        <Hr />
+        <Blog
+          description="While waiting for an answer look at how we helped other teams"
+          blogLinks={[
+            {
+              path: "migrating-an-enterprise-app-from-angularjs-to-react",
+              imageUrl: "/images/from-angular-to-react.png",
+              title: "Migrating an Enterprise App from AngularJS to React",
+            },
+            {
+              path: "how-we-rebuilt-a-legacy-ui-with-zero-downtime",
+              imageUrl:
+                "/images/how-we-rebuilt-a-legacy-ui-with-zero-downtime.png",
+              title:
+                "How We Rebuilt a Legacy UI With Zero Downtime: A Case Study in Component Libraries and Frontend Guidance",
+            },
+            {
+              path: "using-lago-to-create-a-flexible-billing-system",
+              imageUrl:
+                "/images/using-lago-to-create-a-flexible-billing-system-2.png",
+              title: "Using Lago to Create a Flexible Billing System",
+            },
+          ]}
+        />
       </Section>
     </Layout>
   );
